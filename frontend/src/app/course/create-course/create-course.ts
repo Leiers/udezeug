15749ba72e698ed3bfe54efb0f97ef545a5083ee
marvin2k0 <25713103+marvin2k0@users.noreply.github.com>
2026-04-04 01:53:08 +0200

    Add loading spinner and styling improvements for "Create Course" button

--- frontend/src/app/course/create-course/create-course.ts
+++ frontend/src/app/course/create-course/create-course.ts
@@ -4,6 +4,7 @@ import {MatButton} from '@angular/material/button';
 import {form, FormField, maxLength, minLength, required} from '@angular/forms/signals';
 import {CourseService} from '../course-service';
 import {Router} from '@angular/router';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-create-course',
@@ -13,6 +14,7 @@ import {Router} from '@angular/router';
     MatInput,
     MatButton,
     FormField,
+    MatProgressSpinner,
   ],
   templateUrl: './create-course.html',
   styleUrl: './create-course.css',
@@ -28,12 +30,15 @@ export class CreateCourse {
     maxLength(schema.name, 255)
     required(schema.description)
   })
+  protected readonly isLoading = signal<boolean>(false)
 
   protected onCreateCourse(event: SubmitEvent) {
     event.preventDefault()
 
+    this.isLoading.set(true)
+
     this.courseService.createCourse(this.createCourseModel()).subscribe((data) => {
-      this.router.navigate(["course", data.id]).then()
+      this.router.navigate(["course", data.id]).then(() => this.isLoading.set(false))
     })
   }
 }
